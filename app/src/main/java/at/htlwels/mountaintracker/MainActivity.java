04c4783f195c7e65ad27cb89f830c855be4b573f
sarahbruh <sarah.brunmayr@htl-wels.com>
2022-09-29 16:47:54 +0200

    Switch-Button added

--- app/src/main/java/at/htlwels/mountaintracker/MainActivity.java
+++ app/src/main/java/at/htlwels/mountaintracker/MainActivity.java
@@ -2,13 +2,56 @@ package at.htlwels.mountaintracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "at.htlwels.MainActivity";
+    private static final int  MENU_ITEM_ITEM1 = 1;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Main Activity created");
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //menu.add(Menu.NONE, MENU_ITEM_ITEM1, Menu.NONE, "Hello");
+        MenuInflater inflater = this.getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_hello:
+                Log.i(TAG, "Menu Item - Hello - clicked!");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
